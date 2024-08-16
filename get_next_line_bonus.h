3fb80a0b9b8eb1f4698678cad8a8f458dea44393
Kazuki Takada <katakada@student.42tokyo.jp>
2024-08-17 00:49:55 +0900

    Add get_next_line_bonus.h and get_next_line_utils_bonus.c

---
+++ get_next_line_bonus.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/17 00:44:44 by katakada          #+#    #+#             */
+/*   Updated: 2024/08/17 00:47:38 by katakada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+# if BUFFER_SIZE > SIZE_MAX || BUFFER_SIZE < 1
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 0
+#  error "BUFFER_SIZE must be between 1 and SIZE_MAX"
+# endif
+
+char	*get_next_line(int fd);
+int		init_parm(int *num_of_reads, int *is_end, char **oneline,
+			char **read_buf);
+int		fd_read(int fd, char **remain, char **buf);
+char	*get_next_str(char **buf, char **remain, int *flag);
+int		put_oneline(char **result, char *str);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katakada <katakada@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/17 00:45:12 by katakada          #+#    #+#             */
+/*   Updated: 2024/08/17 00:47:11 by katakada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str_src)
+{
+	size_t	str_len;
+
+	if (!str_src)
+		return (0);
+	str_len = 0;
+	while (str_src[str_len])
+		str_len++;
+	return (str_len);
+}
+
+void	*ft_memcpy(void *target_dest, const void *target_src, size_t copy_bytes)
+{
+	unsigned char	*copy_dest;
+	unsigned char	*copy_src;
+	size_t			target_index;
+
+	if (!target_dest || !target_src)
+		return (target_dest);
+	copy_dest = (unsigned char *)target_dest;
+	copy_src = (unsigned char *)target_src;
+	target_index = 0;
+	if (target_dest != target_src)
+	{
+		while (copy_bytes > target_index++)
+			*copy_dest++ = *copy_src++;
+	}
+	return (target_dest);
+}
+
+char	*ft_strdup(const char *str_src)
+{
+	char	*duplicated_new_str;
+	size_t	str_len;
+
+	str_len = ft_strlen(str_src);
+	duplicated_new_str = (char *)malloc(sizeof(char) * (str_len + 1));
+	if (!duplicated_new_str)
+		return (NULL);
+	ft_memcpy(duplicated_new_str, str_src, (str_len + 1));
+	return (duplicated_new_str);
+}
+
+char	*ft_substr(char const *main_str_src, unsigned int start_index,
+		size_t request_substr_len)
+{
+	size_t	main_str_len;
+	size_t	substr_len;
+	char	*substr;
+
+	if (!main_str_src)
+		return (NULL);
+	main_str_len = ft_strlen(main_str_src);
+	substr_len = request_substr_len;
+	if (main_str_len <= start_index)
+		return (ft_strdup(""));
+	if (main_str_len < (start_index + request_substr_len))
+		substr_len = main_str_len - start_index;
+	substr = (char *)malloc(sizeof(char) * (substr_len + 1));
+	if (!substr)
+		return (NULL);
+	ft_memcpy(substr, (main_str_src + start_index), substr_len);
+	substr[substr_len] = '\0';
+	return (substr);
+}
+
+char	*ft_strjoin(char const *str_src_1, char const *str_src_2)
+{
+	char	*join;
+	size_t	len_str1;
+	size_t	len_str2;
+
+	if (!str_src_1 || !str_src_2)
+		return (NULL);
+	len_str1 = ft_strlen(str_src_1);
+	len_str2 = ft_strlen(str_src_2);
+	join = (char *)malloc(sizeof(char) * (len_str1 + len_str2 + 1));
+	if (!join)
+		return (NULL);
+	ft_memcpy(join, str_src_1, len_str1);
+	ft_memcpy(join + len_str1, str_src_2, len_str2 + 1);
+	return (join);
+}
